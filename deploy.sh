d1db34840ad81eeac5738414c4ecfca59966a8cb
Glenn K. Lockwood <glennklockwood@gmail.com>
2016-07-22 22:32:29 -0700

    cleaning up of deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,75 +2,102 @@
 #
 #  Script for cleaning up the mess that Hugo makes and deploying it
 #
-
-### For converting last-modified times
-declare -A months
-months[Jan]="January"
-months[Feb]="February"
-months[Mar]="March"
-months[Apr]="April"
-months[May]="May"
-months[Jun]="June"
-months[Jul]="July"
-months[Aug]="August"
-months[Sep]="September"
-months[Oct]="October"
-months[Nov]="November"
-months[Dec]="December"
-months[Jan]="1"
-months[Feb]="2"
-months[Mar]="3"
-months[Apr]="4"
-months[May]="5"
-months[Jun]="6"
-months[Jul]="7"
-months[Aug]="8"
-months[Sep]="9"
-months[Oct]="10"
-months[Nov]="11"
-months[Dec]="12"
-
-###
-### Figure out where the repo lives
-###
 REPO_HOME="$(git rev-parse --show-toplevel)"
+
+###
+### The master recipe for building this website
+###
+function build_website {
+    set -e
+
+    check_clean_index
+    check_required_binaries
+    check_deploy_environment
+
+    if [ -d "${REPO_HOME}/public" ]; then
+        echo "Removing previous build"
+        rm -rf "${REPO_HOME}/public/"
+    fi
+
+    echo "Recompiling html from markdown"
+    hugo
+
+    ### Fix the last update times since Hugo can't do this sensibly
+    for md_file in $(find ${REPO_HOME}/content/ -type f -name \*.md); do
+        fix_update_time "$md_file"
+    done
+    fix_update_time "GLOBAL"
+
+    ### God only knows where the empty 404.html that Hugo generates is coming from.
+    ### Replace it with the one I made.
+    if [ -f "${REPO_HOME}/public/_404.html" ]; then
+        mv "${REPO_HOME}/public/_404.html" "${REPO_HOME}/public/404.html"
+    fi
+
+    ### Clean up the duplicate x.html and x/index.html files that Hugo makes
+    clean_redundant_htmls
+    set +e
+}
+
+###
+### The master recipe to deploy this website
+###
+function deploy_website {
+    echo "Deploying html"
+    rsync -avr "${REPO_HOME}/public/" "${WEBSITE_REMOTE_USER}@${WEBSITE_REMOTE_HOST}:${WEBSITE_REMOTE_PATH}/"
+}
 
 ###
 ### Required settings to deploy website
 ###
-RSYNC_OPTIONS="-av"
-if [ -z "$WEBSITE_REMOTE_USER" -o -z "$WEBSITE_REMOTE_HOST" -o -z "$WEBSITE_REMOTE_PATH" ]; then
-    echo "You must have the following environment variables defined:"
-    echo "WEBSITE_REMOTE_USER (currently=$WEBSITE_REMOTE_USER)"
-    echo "WEBSITE_REMOTE_HOST (currently=$WEBSITE_REMOTE_HOST)"
-    echo "WEBSITE_REMOTE_PATH (currently=$WEBSITE_REMOTE_PATH)"
-    exit 1
-fi
+function check_deploy_environment {
+    if [ -z "$WEBSITE_REMOTE_USER" -o -z "$WEBSITE_REMOTE_HOST" -o -z "$WEBSITE_REMOTE_PATH" ]; then
+        echo "You must have the following environment variables defined:"
+        echo "    WEBSITE_REMOTE_USER (currently=$WEBSITE_REMOTE_USER)"
+        echo "    WEBSITE_REMOTE_HOST (currently=$WEBSITE_REMOTE_HOST)"
+        echo "    WEBSITE_REMOTE_PATH (currently=$WEBSITE_REMOTE_PATH)"
+        exit 1
+    fi
+}
 
 ###
 ### Make sure pygmentize is installed.
 ### If it isn't available, html will silently come out all broken
 ###
-if ! which pygmentize >/dev/null 2>&1; then
-    echo "pygmentize was NOT found"
-    exit 1
-fi
+function check_required_binaries {
+    for required_bin in hugo pygmentize; do
+        if ! which ${required_bin} >/dev/null 2>&1; then
+            echo "${required_bin} was NOT found"
+            exit 1
+        fi
+    done
+}
 
 ###
 ### We rely on git commit logs to get the last-updated timestamps for each
 ### html file, so refuse to proceed unless our index is clean
 ###
-if ! git diff-index --quiet --exit-code HEAD; then
-    echo "It looks like the git index is not clean, so we cannot correctly" >&2
-    echo "insert the 'Last modified' lines into our generated html.  Please" >&2
-    echo "commit all changes and then re-run this script." >&2
-    exit 1
-fi
+function check_clean_index {
+    if ! git diff-index --quiet --exit-code HEAD; then
+        echo "It looks like the git index is not clean, so we cannot correctly" >&2
+        echo "insert the 'Last modified' lines into our generated html.  Please" >&2
+        echo "commit all changes and then re-run this script." >&2
+        exit 1
+    fi
+}
 
 ###
 ### Update 'Last updated' times
 ###
 function fix_update_time {
+    declare -A months
+    months=( [Jan]="January"    [Feb]="February"    [Mar]="March"
+             [Apr]="April"      [May]="May"         [Jun]="June"
+             [Jul]="July"       [Aug]="August"      [Sep]="September"
+             [Oct]="October"    [Nov]="November"    [Dec]="December" )
+    months=( [Jan]="1"  [Feb]="2"  [Mar]="3"  [Apr]="4"  [May]="5"  [Jun]="6"
+             [Jul]="7"  [Aug]="8"  [Sep]="9"  [Oct]="10" [Nov]="11" [Dec]="12" )
+
     local md_file="$1"
     if [ "$md_file" == "GLOBAL" ]; then
         local html_file="${REPO_HOME}/public/index.html"
@@ -112,33 +139,9 @@ function clean_redundant_htmls {
     done
 }
 
-set -e
 
-if [ -d "${REPO_HOME}/public" ]; then
-    echo "Removing previous build"
-    rm -rf "${REPO_HOME}/public/"
-fi
-
-echo "Recompiling html from markdown"
-hugo
-
-### Fix the last update times since Hugo can't do this sensibly
-for md_file in $(find ${REPO_HOME}/content/ -type f -name \*.md); do
-    fix_update_time "$md_file"
-done
-fix_update_time "GLOBAL"
-
-### God only knows where the empty 404.html that Hugo generates is coming from.
-### Replace it with the one I made.
-if [ -f "${REPO_HOME}/public/_404.html" ]; then
-    mv "${REPO_HOME}/public/_404.html" "${REPO_HOME}/public/404.html"
-fi
-
-### Clean up the duplicate x.html and x/index.html files that Hugo makes
-clean_redundant_htmls
-
-echo "Deploying html"
-
-set -x
-rsync ${RSYNC_OPTIONS} -r "${REPO_HOME}/public/" "${WEBSITE_REMOTE_USER}@${WEBSITE_REMOTE_HOST}:${WEBSITE_REMOTE_PATH}/"
-set +x
+###
+### Actually pull the trigger and run all of this
+###
+build_website
+deploy_website
